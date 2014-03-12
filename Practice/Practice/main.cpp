fad044752417e0c7935f955edb1e9fc1a7aadc69
MichaelBai <bailu1234@gmail.com>
2014-03-12 22:38:14 +0800

    - practice function pointer

--- Practice/Practice/main.cpp
+++ Practice/Practice/main.cpp
@@ -1,31 +1,20 @@
-#include <stdio.h>
-#include <windows.h>
-volatile long g_nLoginCount; //登录次数
-unsigned int __stdcall Fun(void *pPM); //线程函数
-const DWORD THREAD_NUM = 50;//启动线程数
-DWORD WINAPI ThreadFun(void *pPM)
+
+#include <iostream>
+#include <cstdio>
+
+int max(int x, int y)
 {
-    Sleep(100); //some work should to do
-    InterlockedIncrement((LPLONG)&g_nLoginCount);
-    Sleep(50);
-    return 0;
+    return x>y?x:y;
 }
+
+using namespace std;
+
 int main()
 {
-    printf("     原子操作 Interlocked系列函数的使用\n");
-    printf(" -- by MoreWindows( http://blog.csdn.net/MoreWindows ) --\n\n");
-
-    //重复20次以便观察多线程访问同一资源时导致的冲突
-    int num= 20;
-    while (num--)
-    {	
-        g_nLoginCount = 0;
-        int i;
-        HANDLE  handle[THREAD_NUM];
-        for (i = 0; i < THREAD_NUM; i++)
-            handle[i] = CreateThread(NULL, 0, ThreadFun, NULL, 0, NULL);
-        WaitForMultipleObjects(THREAD_NUM, handle, TRUE, INFINITE);
-        printf("有%d个用户登录后记录结果是%d\n", THREAD_NUM, g_nLoginCount);
-    }
+    int max(int, int);
+    int (*p)(int, int) = &max;
+    int a=1,b=2,c=3,d;
+    d=(*p)((*p)(a,b),c);
+    printf("%d\n",d);
     return 0;
 }
