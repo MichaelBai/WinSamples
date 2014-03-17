b5d43e931f4b72ece1b6c036846356f246a4874e
MichaelBai <bailu1234@gmail.com>
2014-03-17 22:15:39 +0800

    - use Event to solve multi-thread synchronization problem

--- Practice/Practice/main.cpp
+++ Practice/Practice/main.cpp
@@ -6,9 +6,15 @@
 long g_nNum; //全局资源
 unsigned int __stdcall Fun(void *pPM); //线程函数
 const int THREAD_NUM = 10; //子线程个数
-
+//事件与关键段
+HANDLE  g_hThreadEvent;
+CRITICAL_SECTION g_csThreadCode;
 int main()
 {
+    //初始化事件和关键段 自动置位,初始无触发的匿名事件  
+    g_hThreadEvent = CreateEvent(NULL, FALSE, FALSE, NULL);   
+    InitializeCriticalSection(&g_csThreadCode);
+
     g_nNum = 0;
     HANDLE handle[THREAD_NUM];
 
@@ -16,20 +22,27 @@ int main()
     while (i < THREAD_NUM) 
     {
         handle[i] = (HANDLE)_beginthreadex(NULL, 0, Fun, &i, 0, NULL);
-        i++;//等子线程接收到参数时主线程可能改变了这个i的值
+        WaitForSingleObject(g_hThreadEvent, INFINITE);
+        i++;
     }
-    //保证子线程已全部运行结束
     WaitForMultipleObjects(THREAD_NUM, handle, TRUE, INFINITE);
+
+    CloseHandle(g_hThreadEvent);
+    DeleteCriticalSection(&g_csThreadCode);
     return 0;
 }
 
 unsigned int __stdcall Fun(void *pPM)
 {
-    //由于创建线程是要一定的开销的，所以新线程并不能第一时间执行到这来
-    int nThreadNum = *(int*)pPM; //子线程获取参数
+    int nThreadNum = *(int*)pPM;
+    SetEvent(g_hThreadEvent);
+
     Sleep(50);//some work should to do
+
+    EnterCriticalSection(&g_csThreadCode);
     g_nNum++;  //处理全局资源
     Sleep(20);//some work should to do
     printf("线程编号为%d  全局资源值为%d\n", nThreadNum, g_nNum);
+    LeaveCriticalSection(&g_csThreadCode);
     return 0;
 }
