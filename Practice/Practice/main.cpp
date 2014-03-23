e846fd10f59c607d56bed770a93531e2e38b25ba
MichaelBai <bailu1234@gmail.com>
2014-03-23 12:00:40 +0800

    - productor and consumer problem

--- Practice/Practice/main.cpp
+++ Practice/Practice/main.cpp
@@ -3,48 +3,72 @@
 #include <process.h>
 #include <windows.h>
 
-long g_nNum; //全局资源
-unsigned int __stdcall Fun(void *pPM); //线程函数
-const int THREAD_NUM = 10; //子线程个数
-//信号量与关键段
-HANDLE  g_hThreadSemaphore;
-CRITICAL_SECTION g_csThreadCode;
-int main()
+const int PROD_NUM = 10;
+const int BUFFER_SIZE = 4;
+HANDLE  g_semaphoreBufferFull, g_semaphoreBufferEmpty;
+CRITICAL_SECTION g_cs;
+int g_buffer[BUFFER_SIZE];
+int g_prodIndex = 0, g_cnsmIndex = 0;
+
+unsigned int __stdcall productor(void *pPM)
 {
-    //初始化信号量和关键段 
-    g_hThreadSemaphore = CreateSemaphore(NULL, 0, 1, NULL);   
-    InitializeCriticalSection(&g_csThreadCode);
-
-    g_nNum = 0;
-    HANDLE handle[THREAD_NUM];
-
-    int i = 0;
-    while (i < THREAD_NUM) 
+    for (int i = 1; i <= PROD_NUM; i++)
     {
-        handle[i] = (HANDLE)_beginthreadex(NULL, 0, Fun, &i, 0, NULL);
-        WaitForSingleObject(g_hThreadSemaphore, INFINITE);
-        i++;
-    }
-    WaitForMultipleObjects(THREAD_NUM, handle, TRUE, INFINITE);
+        WaitForSingleObject(g_semaphoreBufferEmpty, INFINITE);
 
-    CloseHandle(g_hThreadSemaphore);
-    DeleteCriticalSection(&g_csThreadCode);
-    for (i = 0; i < THREAD_NUM; i++)  
-        CloseHandle(handle[i]); 
+        EnterCriticalSection(&g_cs);
+        g_buffer[g_prodIndex] = i;
+        printf("生产者向缓冲区写入%d\n", i);
+        g_prodIndex = (g_prodIndex + 1) % BUFFER_SIZE;
+        LeaveCriticalSection(&g_cs);
+
+        ReleaseSemaphore(g_semaphoreBufferFull, 1, NULL);
+    }
     return 0;
 }
 
-unsigned int __stdcall Fun(void *pPM)
+unsigned int __stdcall consumer(void *pPM)
 {
-    int nThreadNum = *(int*)pPM;
-    ReleaseSemaphore(g_hThreadSemaphore, 1, NULL);
+    while (true)
+    {
+        WaitForSingleObject(g_semaphoreBufferFull, INFINITE);
 
-    Sleep(50);//some work should to do
+        EnterCriticalSection(&g_cs);
+        printf("  消费者%d读出%d\n", GetCurrentThreadId(), g_buffer[g_cnsmIndex]);
+        if (g_buffer[g_cnsmIndex] == PROD_NUM)
+        {
+            // make another consumer thread quit
+            ReleaseSemaphore(g_semaphoreBufferFull, 1, NULL);
+            LeaveCriticalSection(&g_cs);
+            break;
+        }
+        g_cnsmIndex = (g_cnsmIndex + 1) % BUFFER_SIZE;
+        LeaveCriticalSection(&g_cs);
 
-    EnterCriticalSection(&g_csThreadCode);
-    g_nNum++;
-    Sleep(20);//some work should to do
-    printf("线程编号为%d  全局资源值为%d\n", nThreadNum, g_nNum);
-    LeaveCriticalSection(&g_csThreadCode);
+        ReleaseSemaphore(g_semaphoreBufferEmpty, 1, NULL);
+    }
+    return 0;
+}
+
+int main()
+{
+    //初始化信号量和关键段 
+    g_semaphoreBufferFull = CreateSemaphore(NULL, 0, BUFFER_SIZE, NULL);
+    g_semaphoreBufferEmpty = CreateSemaphore(NULL, BUFFER_SIZE, BUFFER_SIZE, NULL);
+    InitializeCriticalSection(&g_cs);
+
+    const int THREAD_NUM = 3;
+    HANDLE handle[THREAD_NUM];
+
+    handle[0] = (HANDLE)_beginthreadex(NULL, 0, productor, NULL, 0, NULL);
+    handle[1] = (HANDLE)_beginthreadex(NULL, 0, consumer, NULL, 0, NULL);
+    handle[2] = (HANDLE)_beginthreadex(NULL, 0, consumer, NULL, 0, NULL);
+    WaitForMultipleObjects(THREAD_NUM, handle, TRUE, INFINITE);
+
+    CloseHandle(g_semaphoreBufferEmpty);
+    CloseHandle(g_semaphoreBufferFull);
+    DeleteCriticalSection(&g_cs);
+    for (int i = 0; i < THREAD_NUM; i++)  
+        CloseHandle(handle[i]); 
     return 0;
 }
